7b2caff238736fb7d77819928a707bf7394ff739
Bob Rudis <bob@rudis.net>
2016-08-12 19:46:47 -0400

    returns almost everything

--- src/filemagic.cpp
+++ src/filemagic.cpp
@@ -6,12 +6,25 @@ using namespace Rcpp;
 
 //' Return file info
 //'
+//' @param path character vector of files to use magic on
+//' @return a \code{tibble} / \code{data.frame} of file magic attributes
 //' @export
+//' @examples
+//' library(magrittr)
+//' library(dplyr)
+//'
+//' system.file("img", package="filemagic") %>%
+//'   list.files(full.names=TRUE) %>%
+//'   incant() %>%
+//'   glimpse()
 // [[Rcpp::export]]
-DataFrame get_mimetype(CharacterVector path) {
+DataFrame incant(CharacterVector path) {
 
   unsigned int input_size = path.size();
   StringVector mime_type(input_size);
+  StringVector encoding(input_size);
+  StringVector extensions(input_size);
+  StringVector description(input_size);
 
   for (unsigned int i=0; i<input_size; i++) {
 
@@ -24,28 +37,73 @@ DataFrame get_mimetype(CharacterVector path) {
     magic_t cookie = magic_open(flags);
 
     if (cookie == NULL) {
-
       mime_type[i] = NA_STRING;
-
     } else {
-
       magic_load(cookie, NULL);
-
       const char *magic_result = magic_file(cookie, fullPath.c_str());
-
       if (magic_result == NULL) {
         mime_type[i] = NA_STRING;
       } else {
         std::string res = std::string(magic_result, strnlen(magic_result, 1024));
         mime_type(i) = res;
       }
-
     }
 
+    flags = MAGIC_MIME_ENCODING;
+    cookie = magic_open(flags);
+    if (cookie == NULL) {
+      encoding[i] = NA_STRING;
+    } else {
+      magic_load(cookie, NULL);
+      const char *magic_result = magic_file(cookie, fullPath.c_str());
+      if (magic_result == NULL) {
+        encoding[i] = NA_STRING;
+      } else {
+        std::string res = std::string(magic_result, strnlen(magic_result, 1024));
+        encoding(i) = res;
+      }
+    }
+
+    flags = MAGIC_EXTENSION;
+    cookie = magic_open(flags);
+    if (cookie == NULL) {
+      extensions[i] = NA_STRING;
+    } else {
+      magic_load(cookie, NULL);
+      const char *magic_result = magic_file(cookie, fullPath.c_str());
+      if (magic_result == NULL) {
+        extensions[i] = NA_STRING;
+      } else {
+        std::string res = std::string(magic_result, strnlen(magic_result, 1024));
+        extensions(i) = res;
+      }
+    }
+
+    flags = MAGIC_NONE;
+    cookie = magic_open(flags);
+    if (cookie == NULL) {
+      description[i] = NA_STRING;
+    } else {
+      magic_load(cookie, NULL);
+      const char *magic_result = magic_file(cookie, fullPath.c_str());
+      if (magic_result == NULL) {
+        description[i] = NA_STRING;
+      } else {
+        std::string res = std::string(magic_result, strnlen(magic_result, 1024));
+        description(i) = res;
+      }
+    }
   }
 
-  return DataFrame::create(_["file"] = path,
-                           _["mime_type"] = mime_type,
-                           _["stringsAsFactors"] = false);
+  DataFrame df = DataFrame::create(_["file"] = path,
+                                   _["mime_type"] = mime_type,
+                                   _["encoding"] = encoding,
+                                   _["extensions"] = extensions,
+                                   _["description"] = description,
+                                   _["stringsAsFactors"] = false);
+
+  df.attr("class") = CharacterVector::create("tbl_df", "tbl", "data.frame");
+
+  return(df);
 
 }
