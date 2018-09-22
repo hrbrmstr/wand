ecb1aa0316ab9f4027240ac0f4ed9ecebd1466c3
boB Rudis <bob@rud.is>
2018-09-22 04:54:00 -0400

    Added better jpeg + new lzip, 7-zip, wasm, pcap, avro, parquet detections

--- tests/testthat/test-wand.R
+++ tests/testthat/test-wand.R
@@ -5,36 +5,56 @@ test_that("Basic file tests work", {
     actions.csv = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     actions.txt = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     actions.xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    test_1.2.class = "application/java-vm", test_1.3.class = "application/java-vm",
-    test_1.4.class = "application/java-vm", test_1.5.class = "application/java-vm",
-    test_128_44_jstereo.mp3 = "audio/mp3", test_excel_2000.xls = "application/msword",
-    test_excel_spreadsheet.xml = "application/xml", test_excel_web_archive.mht = "message/rfc822",
-    test_excel.xlsm = "application/zip", test_excel.xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    test_nocompress.tif = "image/tiff", test_powerpoint.pptm = "application/zip",
+    test_1.2.class = "application/java-vm",
+    test_1.3.class = "application/java-vm",
+    test_1.4.class = "application/java-vm",
+    test_1.5.class = "application/java-vm",
+    test_128_44_jstereo.mp3 = "audio/mp3",
+    test_excel_2000.xls = "application/msword",
+    test_excel_spreadsheet.xml = "application/xml",
+    test_excel_web_archive.mht = "message/rfc822",
+    test_excel.xlsm = "application/zip",
+    test_excel.xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    test_nocompress.tif = "image/tiff",
+    test_powerpoint.pptm = "application/zip",
     test_powerpoint.pptx = "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    test_word_2000.doc = "application/msword", test_word_6.0_95.doc = "application/msword",
-    test_word.docm = "application/zip", test_word.docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    test.au = "audio/basic", test.bin = c(
+    test_word_2000.doc = "application/msword",
+    test_word_6.0_95.doc = "application/msword",
+    test_word.docm = "application/zip",
+    test_word.docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    test.au = "audio/basic",
+    test.bin = c(
       "application/mac-binary",
       "application/macbinary", "application/octet-stream", "application/x-binary",
       "application/x-macbinary"
-    ), test.bmp = "image/bmp", test.dtd = "application/xml-dtd",
-    test.emf = "application/x-msmetafile", test.eps = "application/postscript",
-    test.fli = c("video/flc", "video/fli", "video/x-fli"), test.gif = "image/gif",
-    test.ico = "image/x-icon", test.java = c(
+    ), test.bmp = "image/bmp",
+    test.dtd = "application/xml-dtd",
+    test.emf = "application/x-msmetafile",
+    test.eps = "application/postscript",
+    test.fli = c("video/flc", "video/fli", "video/x-fli"),
+    test.gif = "image/gif",
+    test.ico = "image/x-icon",
+    test.java = c(
       "text/plain", "text/x-java",
       "text/x-java-source"
-    ), test.jpg = "image/jpeg", test.mp3 = "audio/mp3",
-    test.odt = "application/vnd.oasis.opendocument.text", test.ogg = c(
+    ), test.jpg = "image/jpeg",
+    test.mp3 = "audio/mp3",
+    test.odt = "application/vnd.oasis.opendocument.text",
+    test.ogg = c(
       "application/ogg",
       "audio/ogg"
-    ), test.pcx = c("image/pcx", "image/x-pcx"), test.pdf = "application/pdf",
-    test.pl = c("text/plain", "text/x-perl", "text/x-script.perl"), test.png = "image/png", test.pnm = c(
+    ), test.pcx = c("image/pcx", "image/x-pcx"),
+    test.pdf = "application/pdf",
+    test.pl = c("text/plain", "text/x-perl", "text/x-script.perl"),
+    test.png = "image/png",
+    test.pnm = c(
       "application/x-portable-anymap",
       "image/x-portable-anymap"
     ), test.ppm = "image/x-portable-pixmap",
-    test.ppt = "application/msword", test.ps = "application/postscript",
-    test.psd = "image/photoshop", test.py = c(
+    test.ppt = "application/msword",
+    test.ps = "application/postscript",
+    test.psd = "image/photoshop",
+    test.py = c(
       "text/x-python",
       "text/x-script.phyton"
     ), test.rtf = c(
@@ -44,16 +64,21 @@ test_that("Basic file tests work", {
       "application/x-bsh",
       "application/x-sh", "application/x-shar", "text/x-script.sh",
       "text/x-sh"
-    ), test.tar = "application/pax", test.tar.gz = c(
+    ), test.tar = "application/tar",
+    test.tar.gz = c(
       "application/octet-stream",
       "application/x-compressed", "application/x-gzip"
     ), test.tga = "image/x-tga",
-    test.txt = "text/plain", test.txt.gz = c(
+    test.txt = "text/plain",
+    test.txt.gz = c(
       "application/octet-stream",
       "application/x-compressed", "application/x-gzip"
     ), test.wav = "audio/x-wav",
-    test.wmf = c("application/x-msmetafile", "windows/metafile"), test.xcf = "application/x-xcf", test.xml = "application/xml",
-    test.xpm = c("image/x-xbitmap", "image/x-xpixmap", "image/xpm"), test.zip = "application/zip"
+    test.wmf = c("application/x-msmetafile", "windows/metafile"),
+    test.xcf = "application/x-xcf",
+    test.xml = "application/xml",
+    test.xpm = c("image/x-xbitmap", "image/x-xpixmap", "image/xpm"),
+    test.zip = "application/zip"
   ) -> results
 
   fils <- list.files(system.file("extdat", package="wand"), full.names=TRUE)
